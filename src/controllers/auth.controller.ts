d1cc535be41766e574eaaf3e24acf599767b9f7c
Iqbal Ghozy <iqbal.gozy@gmail.com>
2025-08-22 10:20:05 +0700

    Implement authentication with JWT, including user registration and login functionality

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { AuthService } from "../services/auth.service";
+
+const authService = new AuthService();
+
+export class AuthController {
+  static async register(req: Request, res: Response) {
+    try {
+      const { email, password, confPassword, name, phone } = req.body;
+      const user = await authService.register(
+        email,
+        password,
+        confPassword,
+        name,
+        phone
+      );
+      res.status(201).json({ success: true, user });
+    } catch (error) {
+      res
+        .status(400)
+        .json({ success: false, message: "Internal server error" });
+    }
+  }
+
+  static async login(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+      const { token, user } = await authService.login(email, password);
+      res.status(200).json({ success: true, token, user });
+    } catch (err: any) {
+      res.status(400).json({ success: false, message: err.message });
+    }
+  }
+}
