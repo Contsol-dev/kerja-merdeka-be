d9b8a59f9b7cddebed6c4a1614563205a6c4d7f3
Iqbal Ghozy <iqbal.gozy@gmail.com>
2025-08-22 19:55:43 +0700

    feat: remove Log table and related references from the database schema; add logging to user-related operations

--- src/controllers/job.controller.ts
+++ src/controllers/job.controller.ts
@@ -3,6 +3,7 @@ import { AuthRequest } from "../interfaces/interface";
 import { JobService } from "../services/job.service";
 import { UserService } from "../services/user.service";
 import { InsertJobDataReq } from "../interfaces/dto.interface";
+import logger from "../lib/logger";
 
 const jobService = new JobService();
 const userService = new UserService();
@@ -12,7 +13,15 @@ export class JobController {
     const userId = userService.checkCurrentUser(req);
     const jobData: InsertJobDataReq = req.body;
 
+    logger.info(
+      `(INSERT JOB) - User ID: ${userId}, Job Data: ${jobData.jobTitle} - ${jobData.company} Attempting to insert job data`
+    );
+
     const newJobData = await jobService.insertJobData(userId, jobData);
+
+    logger.info(
+      `(INSERT JOB) - User ID: ${userId}, Job Data: ${jobData.jobTitle} - ${jobData.company} Successfully inserted job data`
+    );
 
     res.status(201).json({
       success: true,
@@ -23,7 +32,15 @@ export class JobController {
   static async getJobList(req: AuthRequest, res: Response) {
     const userId = userService.checkCurrentUser(req);
 
+    logger.info(
+      `(GET JOB LIST) - User ID: ${userId} Attempting to retrieve job list`
+    );
+
     const jobList = await jobService.getJobList(userId);
+
+    logger.info(
+      `(GET JOB LIST) - User ID: ${userId} Successfully retrieved job list`
+    );
 
     res.status(200).json({
       success: true,
--- src/controllers/mock-interview.controller.ts
+++ src/controllers/mock-interview.controller.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 import { MockInterviewService } from "../services/mock-interview.service";
 import { AuthRequest } from "../interfaces/interface";
 import { UserService } from "../services/user.service";
+import logger from "../lib/logger";
 
 const service = new MockInterviewService();
 const userService = new UserService();
@@ -10,7 +11,16 @@ export const startInterview = async (req: AuthRequest, res: Response) => {
   const { jobDataId } = req.params;
   const userId = userService.checkCurrentUser(req);
 
+  logger.info(
+    `(START INTERVIEW) - User ID: ${userId}, Job Data ID: ${jobDataId} Attempting to start interview`
+  );
+
   const result = await service.startInterview(userId, jobDataId);
+
+  logger.info(
+    `(START INTERVIEW) - User ID: ${userId}, Job Data ID: ${jobDataId} Successfully started interview`
+  );
+
   return res.status(201).json({
     success: true,
     data: result,
@@ -20,11 +30,19 @@ export const startInterview = async (req: AuthRequest, res: Response) => {
 export const answerInterview = async (req: AuthRequest, res: Response) => {
   const { jobDataId } = req.params;
   const { answer } = req.body;
-
   const userId = userService.checkCurrentUser(req);
+
+  logger.info(
+    `(ANSWER INTERVIEW) - User ID: ${userId}, Job Data ID: ${jobDataId} Attempting to answer interview question`
+  );
 
   await service.userJobCheck(userId, jobDataId);
   const result = await service.answerQuestion(jobDataId, answer);
+
+  logger.info(
+    `(ANSWER INTERVIEW) - User ID: ${userId}, Job Data ID: ${jobDataId} Successfully answered interview question`
+  );
+
   return res.status(201).json({
     success: true,
     data: result,
@@ -35,8 +53,17 @@ export const getInterviewLogs = async (req: AuthRequest, res: Response) => {
   const { jobDataId } = req.params;
   const userId = userService.checkCurrentUser(req);
 
+  logger.info(
+    `(GET INTERVIEW LOGS) - User ID: ${userId}, Job Data ID: ${jobDataId} Attempting to retrieve interview logs`
+  );
+
   await service.userJobCheck(userId, jobDataId);
   const logs = await service.getInterviewLogs(jobDataId);
+
+  logger.info(
+    `(GET INTERVIEW LOGS) - User ID: ${userId}, Job Data ID: ${jobDataId} Successfully retrieved interview logs`
+  );
+
   return res.status(200).json({
     success: true,
     data: logs,
@@ -47,8 +74,17 @@ export const getInterviewFeedback = async (req: AuthRequest, res: Response) => {
   const { jobDataId } = req.params;
   const userId = userService.checkCurrentUser(req);
 
+  logger.info(
+    `(GET INTERVIEW FEEDBACK) - User ID: ${userId}, Job Data ID: ${jobDataId} Attempting to retrieve interview feedback`
+  );
+
   await service.userJobCheck(userId, jobDataId);
   const feedback = await service.getInterviewFeedback(jobDataId);
+
+  logger.info(
+    `(GET INTERVIEW FEEDBACK) - User ID: ${userId}, Job Data ID: ${jobDataId} Successfully retrieved interview feedback`
+  );
+
   return res.status(200).json({
     success: true,
     data: feedback,
